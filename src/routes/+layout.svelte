8311cc763982ae9fa892d00d9b6072519c284eff
Lewis Crichton <lewistehminerz@outlook.com>
2022-11-11 18:39:26 +0000

    add the starts of the real site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,33 @@
     import "../app.css";
 </script>
 
-<main><slot/></main>
+<div id="content">
+    <aside id="columnLeft"><slot name="columnLeft"/></aside>
+    <div id="mainContent">
+        <header>
+            <nav><img src="/logo.svg" alt="VIVY logo" height="30"></nav>
+        </header>
+        <main><slot/></main>
+    </div>
+    <aside id="columnRight"><slot name="columnRight"/></aside>
+</div>
+
+<footer/>
 
 <style>
-    main {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
+    #content {
+        display: flex;
+    }
+
+    #columnLeft {
+        flex: 20%;
+    }
+
+    #columnRight {
+        flex: 20%;
+    }
+
+    #mainContent {
+        flex: 60%;
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,3 @@
 <svelte:head>
     <title>VIVY</title>
 </svelte:head>
-
-<img src="/logo.svg" alt="VIVY logo" height="100">
